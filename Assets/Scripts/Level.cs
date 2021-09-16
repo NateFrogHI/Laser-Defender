26d67d9162ff91184938d693d684a0e1fd9aed63
NateFrogHI <n.k.mitson@gmail.com>
2021-09-16 00:26:19 -0700

    Loading Scenes

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    private enum Scene
+    {
+        Start,
+        Game,
+        Lose
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene((int) Scene.Lose);
+    }
+
+    public void LoadGameScene() {
+        SceneManager.LoadScene((int) Scene.Game);
+    }
+
+    public void LoadStartMenu() {
+        SceneManager.LoadScene((int) Scene.Start);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
